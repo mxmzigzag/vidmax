f11c6619a58776ced5174911f82de3e1858bf942
mxmzigzag <qzzik97@gmail.com>
2023-04-14 23:54:00 +0300

    changed logo to real

--- client/src/components/MainBanner/MainBanner.tsx
+++ client/src/components/MainBanner/MainBanner.tsx
@@ -1,6 +1,7 @@
 import React, { MutableRefObject, useRef } from "react";
 
 import Banner from "../../assets/images/MainBanner.jpeg";
+import LogoIcon from "../../assets/icons/logo";
 
 function MainBanner() {
   const blockRef = useRef<null | HTMLDivElement>(null);
@@ -15,10 +16,7 @@ function MainBanner() {
   return (
     <div className="h-screen bg-vBlack relative overflow-hidden" ref={blockRef}>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30">
-        <h1 className="text-8xl text-white text-center">VIDMAX</h1>
-        <h2 className="text-3xl text-white text-center tracking-[40px] indent-[45px]">
-          CINEMA
-        </h2>
+        <LogoIcon width={350} />
       </div>
       <button
         className="text-xl font-light text-white tracking-widest absolute bottom-10 left-1/2 -translate-x-1/2 z-30"
